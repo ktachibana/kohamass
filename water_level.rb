c8c8a7c013908ac773c63768247c5233610007f9
Kenichi Tachibana <deadzone@be.to>
2019-07-14 15:47:04 +0900

    ページ構造が変わって読めていなかったのを修正

--- water_level.rb
+++ water_level.rb
@@ -10,14 +10,15 @@ class WaterLevel
 
   def self.load!
     doc = Nokogiri::HTML(open(URL))
-    year_cell = doc.css('td:contains("今年")').last
-    year = year_cell.text[/(\d+).*年/].to_i
-    month = doc.css('td:contains("の水位")').last.text[/(\d+).*月/].to_i
 
-    levels = year_cell.ancestors('table').first.css('tr').map do |tr|
+    year = Date.current.year
+    content_elm = doc.css('#maincontent')
+    month = content_elm.css('h1:contains("の水位")').first.text[/(\d+)月/, 1]&.to_i || raise('month value was not found.')
+
+    levels = content_elm.css('table').first.css('tr').map do |tr|
       tds = tr.css('td') || next
       cell_texts = tds.map(&:text)
-      next unless cell_texts.size == 3
+      next unless cell_texts.size == 4
 
       day = cell_texts[0].match(/(\d+)日/).try { |match| match[1].to_i } || next
       value = cell_texts[1].match(/\-?\d+/).try { |match| match[0].to_i } || next
